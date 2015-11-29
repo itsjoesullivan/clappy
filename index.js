8eb9537b48f985c463c438ddac22c4ee625b455d
itsjoesullivan <itsjoesullivan@gmail.com>
2015-11-29 12:39:14 -0500

    use new interface

--- index.js
+++ index.js
@@ -1,79 +1,82 @@
 var NoiseBuffer = require('noise-buffer');
 
 module.exports = function(context) {
-  // A few parameters
-  // Overall length of the clap
-  var length = 1.2;
-  // Number of clappers
-  var cycles = 3;
-  // How closely together the clappers clap
-  var clapFrequency = 80;
-  // How long a clap lasts
-  var clapLength = cycles / clapFrequency;
 
-  // This is what we'll eventually return.
-  var audioNode = context.createGain();
+  return function() {
+    // A few parameters
+    // Overall length of the clap
+    var length = 1.2;
+    // Number of clappers
+    var cycles = 3;
+    // How closely together the clappers clap
+    var clapFrequency = 80;
+    // How long a clap lasts
+    var clapLength = cycles / clapFrequency;
 
-  var noise = context.createBufferSource();
-  noise.buffer = NoiseBuffer(length);
+    // This is what we'll eventually return.
+    var audioNode = context.createGain();
 
-  var clapsGain = context.createGain();
-  var clapDecay = context.createGain();
+    var noise = context.createBufferSource();
+    noise.buffer = NoiseBuffer(length);
 
-  noise.connect(clapsGain);
-  clapsGain.connect(clapDecay);
+    var clapsGain = context.createGain();
+    var clapDecay = context.createGain();
 
-  var posGain = context.createGain();
-  var negGain = context.createGain();
-  var lfoGain = context.createGain();
-  negGain.gain.value = 1;
-  negGain.connect(lfoGain);
+    noise.connect(clapsGain);
+    clapsGain.connect(clapDecay);
 
-  clapDecay.connect(negGain);
+    var posGain = context.createGain();
+    var negGain = context.createGain();
+    var lfoGain = context.createGain();
+    negGain.gain.value = 1;
+    negGain.connect(lfoGain);
 
-  var longGain = context.createGain();
-  noise.connect(longGain);
+    clapDecay.connect(negGain);
 
-  var lfo  = context.createOscillator();
-  lfo.type = "sawtooth";
-  lfo.frequency.value = -clapFrequency;
-  lfo.connect(lfoGain.gain);
+    var longGain = context.createGain();
+    noise.connect(longGain);
 
-  var bandpass = context.createBiquadFilter();
-  bandpass.type = "bandpass";
-  longGain.connect(bandpass);
+    var lfo  = context.createOscillator();
+    lfo.type = "sawtooth";
+    lfo.frequency.value = -clapFrequency;
+    lfo.connect(lfoGain.gain);
 
-  lfoGain.connect(bandpass);
-  posGain.connect(bandpass);
+    var bandpass = context.createBiquadFilter();
+    bandpass.type = "bandpass";
+    longGain.connect(bandpass);
 
-  var highpass = context.createBiquadFilter();
-  highpass.type = "highpass";
-  bandpass.connect(highpass);
-  highpass.connect(context.destination);
+    lfoGain.connect(bandpass);
+    posGain.connect(bandpass);
 
-  bandpass.frequency.value = 800;
-  bandpass.Q.value = 0.7;
-  highpass.frequency.value = 600;
+    var highpass = context.createBiquadFilter();
+    highpass.type = "highpass";
+    bandpass.connect(highpass);
+    highpass.connect(context.destination);
 
-  audioNode.start = function(when) {
-    clapDecay.gain.setValueAtTime(0.0001, when);
-    clapDecay.gain.exponentialRampToValueAtTime(1, when + 0.001);
-    clapDecay.gain.linearRampToValueAtTime(1, when + clapLength);
-    clapDecay.gain.exponentialRampToValueAtTime(0.0001, when + clapLength + 0.01);
+    bandpass.frequency.value = 800;
+    bandpass.Q.value = 0.7;
+    highpass.frequency.value = 600;
 
-    longGain.gain.setValueAtTime(0.0001, when);
-    longGain.gain.setValueAtTime(0.0001, when + clapLength);
-    longGain.gain.exponentialRampToValueAtTime(1, when + clapLength + 0.001);
-    longGain.gain.exponentialRampToValueAtTime(0.2, when + 0.1);
-    longGain.gain.exponentialRampToValueAtTime(0.0001, when + length);
+    audioNode.start = function(when) {
+      clapDecay.gain.setValueAtTime(0.0001, when);
+      clapDecay.gain.exponentialRampToValueAtTime(1, when + 0.001);
+      clapDecay.gain.linearRampToValueAtTime(1, when + clapLength);
+      clapDecay.gain.exponentialRampToValueAtTime(0.0001, when + clapLength + 0.01);
 
-    lfo.start(when);
-    noise.start(when);
+      longGain.gain.setValueAtTime(0.0001, when);
+      longGain.gain.setValueAtTime(0.0001, when + clapLength);
+      longGain.gain.exponentialRampToValueAtTime(1, when + clapLength + 0.001);
+      longGain.gain.exponentialRampToValueAtTime(0.2, when + 0.1);
+      longGain.gain.exponentialRampToValueAtTime(0.0001, when + length);
+
+      lfo.start(when);
+      noise.start(when);
+    };
+
+    audioNode.stop = function(when) {
+      noise.stop(when);
+    };
+
+    return audioNode;
   };
-
-  audioNode.stop = function(when) {
-    noise.stop(when);
-  }
-
-  return audioNode;
 };
--- readme.md
+++ readme.md
@@ -3,13 +3,16 @@
 `npm install --save clappy`
 
 ```javascript
-var clappy = require('clappy');
+var Clappy = require('clappy');
 
 // Initialize AudioContext
 var context = new AudioContext();
 
+// Initialize clap instrument
+var clap = Clappy(context);
+
 // Create clap audio node (one time use only)
-var clapNode = clappy(context);
+var clapNode = clap();
 
 // Connect to target node
 clapNode.connect(context.destination);
