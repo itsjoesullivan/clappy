2aa7155143e73ad37c170b4cbe423738d0b9e1db
Joe Sullivan <itsjoesullivan@gmail.com>
2016-05-17 10:39:07 -0400

    Move shared graph to constructor

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 var NoiseBuffer = require('noise-buffer');
+var buffer = NoiseBuffer(1);
 
 module.exports = function(context) {
+
+  var duration = 1.2;
+  var cycles = 3;
+  var clapFrequency = 80;
+  var clapLength = cycles / clapFrequency;
+
+  var bandpass = context.createBiquadFilter();
+  bandpass.type = "bandpass";
+  bandpass.frequency.value = 800;
+  bandpass.Q.value = 0.7;
+
+  var highpass = context.createBiquadFilter();
+  highpass.type = "highpass";
+  highpass.frequency.value = 600;
+  bandpass.connect(highpass);
 
   return function() {
 
@@ -15,34 +31,25 @@ module.exports = function(context) {
       room.
     */
 
-    var duration = 1.2;
-    var cycles = 3;
-    var clapFrequency = 80;
-    var clapLength = cycles / clapFrequency;
 
     var audioNode = context.createGain();
 
     var noise = context.createBufferSource();
-    noise.buffer = NoiseBuffer(duration);
+    noise.buffer = buffer;
+    noise.loop = true;
 
     var clapDryEnvelope = context.createGain();
+    clapDryEnvelope.gain.value = 0;
 
     var clapDecayEnvelope = context.createGain();
+    clapDecayEnvelope.gain.value = 0;
 
     var lfoCarrier = context.createGain();
-    var lfo  = context.createOscillator();
+    var lfo = context.createOscillator();
     lfo.type = "sawtooth";
     lfo.frequency.value = -clapFrequency;
     lfo.connect(lfoCarrier.gain);
 
-
-    var bandpass = context.createBiquadFilter();
-    bandpass.type = "bandpass";
-    var highpass = context.createBiquadFilter();
-    highpass.type = "highpass";
-    bandpass.frequency.value = 800;
-    bandpass.Q.value = 0.7;
-    highpass.frequency.value = 600;
 
     noise.connect(clapDryEnvelope);
     clapDryEnvelope.connect(lfoCarrier);
@@ -51,7 +58,6 @@ module.exports = function(context) {
     noise.connect(clapDecayEnvelope);
     clapDecayEnvelope.connect(bandpass);
 
-    bandpass.connect(highpass);
     highpass.connect(audioNode);
 
     audioNode.start = function(when) {
@@ -59,24 +65,34 @@ module.exports = function(context) {
       clapDryEnvelope.gain.setValueAtTime(0.0001, when);
       clapDryEnvelope.gain.exponentialRampToValueAtTime(1, when + 0.001);
       clapDryEnvelope.gain.linearRampToValueAtTime(1, when + clapLength);
-      clapDryEnvelope.gain.exponentialRampToValueAtTime(0.0001, when + clapLength + 0.01);
+      clapDryEnvelope.gain.exponentialRampToValueAtTime(0.000000001, when + clapLength + 0.01);
+      clapDryEnvelope.gain.setValueAtTime(0, when + clapLength + 0.02);
 
       clapDecayEnvelope.gain.setValueAtTime(0.0001, when);
       clapDecayEnvelope.gain.setValueAtTime(0.0001, when + clapLength);
       clapDecayEnvelope.gain.exponentialRampToValueAtTime(1, when + clapLength + 0.001);
       clapDecayEnvelope.gain.exponentialRampToValueAtTime(0.2, when + 0.1);
-      clapDecayEnvelope.gain.exponentialRampToValueAtTime(0.0001, when + duration);
+      clapDecayEnvelope.gain.exponentialRampToValueAtTime(0.000000001, when + duration);
+      clapDecayEnvelope.gain.setValueAtTime(0, when + duration + 0.01);
 
       lfo.start(when);
-      noise.start(when);
-
-      audioNode.stop(when + duration);
+      lfo.stop(when + duration);
+      noise.start(when, Math.random() * noise.buffer.duration);
+      noise.stop(when + duration);
+      audioNode.gain.setValueAtTime(0, when + duration);
     };
 
     audioNode.stop = function(when) {
-
-      lfo.stop(when);
-      noise.stop(when);
+      try {
+        lfo.stop(when);
+      } catch(e) {
+        // likely already stopped
+      }
+      try {
+        noise.stop(when);
+      } catch(e) {
+        // likely already stopped
+      }
     };
 
     return audioNode;
